5393f782136d4f32795aba713fe25b2c15e504c3
Mahakhalil <mahameme931@gmail.com>
2024-09-28 20:31:53 +0300

    taskEF

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Createproduct.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Createproduct.Context
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=EF_Task;Trusted_connection=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Createproduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
